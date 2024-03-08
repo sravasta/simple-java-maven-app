d33caa8f076c201ff5735f8bebf00c8d3caad767
Sravanti Bhandaram <sravantib@gmail.com>
2024-03-08 10:59:43 +0100

    newregex

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,7 +19,7 @@ echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 #VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
-VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | grep -E "^[0-9]+\.[0-9]+.*")
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | grep -E "^[0-9]+\.[0-9]+-[A-Za-z]*")
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
